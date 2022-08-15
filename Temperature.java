c5c59edef4e430a51ff14a5aff2325807a9979f4
Sanjithj <sanjithj591997@gmail.com>
2022-08-15 13:55:32 +0530

    Added: Celcius to fahrenhiet

---
+++ Temperature.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class Temperature {
+	
+	 public static void main (String args[])  
+	    { 
+		 float Fahrenheit, Celsius;  
+	          Celsius= 13;  
+	          Fahrenheit =((Celsius*9)/5)+32;  
+	          System.out.println("Fahrenheit is: "+Fahrenheit);  
+	    }  
+
+}
